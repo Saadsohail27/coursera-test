56c8bce4bf9c15f296eefd0c492d0eff17e736c0
Saadsohail27 <saadsohail8790@gmail.com>
2025-01-06 14:06:26 +0500

    writexz update

--- my-personal-websites/writexz/js/header-script.js
+++ my-personal-websites/writexz/js/header-script.js
@@ -1,6 +1,6 @@
 document.addEventListener('scroll', () => {
     const header = document.querySelector('.header');
-    if (window.scrollY > 150) {
+    if (window.scrollY > -20) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
