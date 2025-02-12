2c22c900adfd46f88dfb7595481674df0aac3244
Saadsohail27 <saadsohail8790@gmail.com>
2025-02-12 18:12:30 +0500

    updates

--- my-personal-websites/visa-vasool/js/header-script.js
+++ my-personal-websites/visa-vasool/js/header-script.js
@@ -1,11 +1,14 @@
-// Get the header element
+// Get the header and logo elements
 const header = document.querySelector("header");
+const logo = document.querySelector("header a img");
 
 // Function to check scroll position
 window.addEventListener("scroll", () => {
     if (window.scrollY > 200) {
         header.classList.add("sticky");
+        logo.src = "images/logos/logo-black-80.png"; // Change logo in scrolled state
     } else {
         header.classList.remove("sticky");
+        logo.src = "images/logos/alogo.png"; // Revert to original logo
     }
 });
